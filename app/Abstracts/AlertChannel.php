d4365e0bd2f5a3bcef1ca862fafdf7809e7126b1
jhaskell13 <j.e.haskell1998@gmail.com>
2025-05-09 13:28:29 -0400

    added rate limiting to alert channels

--- app/Abstracts/AlertChannel.php
+++ app/Abstracts/AlertChannel.php
@@ -6,12 +6,19 @@ use App\Contracts\AlertChannelInterface;
 use App\Notifications\GitHubStatusNotification;
 use App\Notifications\Notifiable\Recipient;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\RateLimiter;
 
 abstract class AlertChannel implements AlertChannelInterface
 {
     public function send(string $message, array $context = []): bool
     {
         try {
+            // Rate limit alerts to 5 attempts per channel per 2 minute window
+            if (RateLimiter::tooManyAttempts($key = "alert:{$this->key()}", $maxAttempts = 5)) {
+                return false;
+            }
+
+            RateLimiter::hit($key, $decay = 120);
             Notification::send(new Recipient, new GithubStatusNotification($message, $context));
         } catch (\Exception $e) {
             throw new \Exception("Failed to alert [{$this->key()}] channel: [{$e->getMessage()}]");
